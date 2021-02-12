21d7eb534cf5026c8c8200d1b7e5e14d802127fc
hiroto-hash <hiroto5118@icloud.com>
2021-02-12 16:03:45 +0900

    forwardSample

--- example/src/servlet/ForwardSampleServlet.java
+++ example/src/servlet/ForwardSampleServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class FormSampleServlet
  */
-@WebServlet("/FormSampleServlet")
+@WebServlet("/ForwardSampleServlet")
 public class ForwardSampleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
