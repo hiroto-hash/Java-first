1dbd802e79fb89d0010ee1901d3539337ca32daf
hiroto-hash <hiroto5118@icloud.com>
2021-02-16 15:22:51 +0900

    細かい修正

--- example/src/model/RegisterUserLogic.java
+++ example/src/model/RegisterUserLogic.java
@@ -3,7 +3,6 @@ package model;
 
 //ユーザー登録を行うモデル
 public class RegisterUserLogic {
-
 	public boolean execute(User user) {
 		// TODO 自動生成されたコンストラクター・スタブ
 		return true;
--- example/src/model/User.java
+++ example/src/model/User.java
@@ -1,21 +1,21 @@
 package model;
 
+import java.io.Serializable;
 
 //登録するユーザーを表すJavaBeans
-public class User {
+public class User implements Serializable{
   private String id;
   private String name;
   private String pass;
-  
+
 	public User() {}
 	public User(String id, String name, String pass) {
 		this.id = id;
 		this.name = name;
 		this.pass = pass;
 	}
-	
+
 	public String getId() { return id; }
 	public String getName() { return name; }
 	public String getPass() { return pass; }
-
 }
